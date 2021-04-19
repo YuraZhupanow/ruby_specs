e8bc1b11553c404310868d0db86f878f6447c738
yuzzi <yura.zhupanow@gmail.com>
2021-04-20 01:46:59 +0300

    added headless

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,17 @@ require_all 'spec/support'
 include UserHelper
 include ApiWrapper
 
+if ENV['HEADLESS'] == 'true'
+  require 'headless'
+
+  headless = Headless.new
+  headless.start
+
+  at_exit do
+    headless.destroy
+  end
+end
+
 RSpec.configure do
   def options
     Selenium::WebDriver::Chrome::Options.new(args: %w[window-size=1800,1000])
